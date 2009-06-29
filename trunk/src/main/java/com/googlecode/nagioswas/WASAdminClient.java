c1dc5eaa4595b7275e2159e23f29e3126a8848dc
niklas@protocol7.com <niklas@protocol7.com@0b398506-4074-11de-af2b-f15df6ab9c44>
2009-06-29 09:08:30 +0000

    Adding support for PKCS12 keystores

--- trunk/src/main/java/com/googlecode/nagioswas/WASAdminClient.java
+++ trunk/src/main/java/com/googlecode/nagioswas/WASAdminClient.java
@@ -15,7 +15,6 @@ import com.ibm.websphere.management.AdminClientFactory;
 import com.ibm.websphere.management.exception.ConnectorException;
 
 public class WASAdminClient {
-    private boolean connector_security_enabled = false;
 
     public AdminClient create(String profile) throws ConnectorException {
         AdminClient adminClient;
@@ -27,22 +26,6 @@ public class WASAdminClient {
         props.setProperty(AdminClient.CONNECTOR_PORT, Config.getString(profile, "port"));
         props.setProperty(AdminClient.CACHE_DISABLED, "false");
 
-        if (connector_security_enabled) {
-            try {
-                adminClient = AdminClientFactory.createAdminClient(props);
-                return adminClient;
-            } catch(ConnectorException e) {
-                System.err.println("Could not connect to a SSL SOAP port, trying to set socket factories");
-                // huge hack! Try setting the SSL provider
-                Security.setProperty("ssl.SocketFactory.provider", "com.ibm.jsse2.SSLSocketFactoryImpl");
-                Security.setProperty("ssl.ServerSocketFactory.provider", "com.ibm.jsse2.SSLServerSocketFactoryImpl");
-                
-                adminClient = AdminClientFactory.createAdminClient(props);
-                return adminClient;                
-            }
-        }
-
-        
         if(Config.getBoolean(profile, "securityenabled")) {
             props.setProperty(AdminClient.CONNECTOR_SECURITY_ENABLED, "true");
             props.setProperty(AdminClient.CONNECTOR_AUTO_ACCEPT_SIGNER, "true");
@@ -52,9 +35,12 @@ public class WASAdminClient {
             
             String sslTrustStorePassword = Config.getString(profile, "truststorepassword");
             String sslKeyStorePassword = Config.getString(profile, "keystorepassword");
+
+            String sslTrustStoreType = getStoreType(profile, "trust", sslTrustStore);
+            String sslKeyStoreType = getStoreType(profile, "key", sslKeyStore);
             
-            checkStore(sslTrustStore, sslTrustStorePassword, "Trust");
-            checkStore(sslKeyStore, sslKeyStorePassword, "Key");
+            checkStore(sslTrustStore, sslTrustStorePassword,sslTrustStoreType, "Trust");
+            checkStore(sslKeyStore, sslKeyStorePassword, sslKeyStoreType, "Key");
     
             props.setProperty("javax.net.ssl.trustStore", sslTrustStore
                     .getAbsolutePath());
@@ -62,23 +48,65 @@ public class WASAdminClient {
                     .getAbsolutePath());
             props.setProperty("javax.net.ssl.trustStorePassword", sslTrustStorePassword);
             props.setProperty("javax.net.ssl.keyStorePassword", sslKeyStorePassword);
-    
+
+            props.setProperty("javax.net.ssl.trustStoreType", sslTrustStoreType);
+            props.setProperty("javax.net.ssl.keyStoreType", sslKeyStoreType);
+
+            
             props.setProperty(AdminClient.USERNAME, Config.getString(profile, "username"));
             props.setProperty(AdminClient.PASSWORD, Config.getString(profile, "password"));
         } else {
             props.setProperty(AdminClient.CONNECTOR_SECURITY_ENABLED, "false");
         }
         
-        adminClient = AdminClientFactory.createAdminClient(props);
-
-        return adminClient;
+        try {
+            adminClient = AdminClientFactory.createAdminClient(props);
+            return adminClient;
+        } catch(ConnectorException e) {
+            System.err.println("Could not connect to a SSL SOAP port, trying to set socket factories");
+            // huge hack! Try setting the SSL provider
+            Security.setProperty("ssl.SocketFactory.provider", "com.ibm.jsse2.SSLSocketFactoryImpl");
+            Security.setProperty("ssl.ServerSocketFactory.provider", "com.ibm.jsse2.SSLServerSocketFactoryImpl");
+            
+            adminClient = AdminClientFactory.createAdminClient(props);
+            return adminClient;                
+        }
     }
     
-    private void checkStore(File store, String password, String type) {
+    private String getStoreType(String profile, String type, File store) {
+        String storeType = Config.getString(profile, type + "storetype");
+        
+        // if not configured by the user, look at the file ending
+        if(storeType == null && store != null) {
+            int fileTypeStart = store.getName().lastIndexOf(".");
+            
+            if(fileTypeStart > 0) {
+                String fileType = store.getName().substring(fileTypeStart +1 );
+
+                // map file type into keystore type
+                if(fileType.equalsIgnoreCase("p12")) {
+                    storeType = "pkcs12";
+                } else if(fileType.equalsIgnoreCase("jks")) {
+                    storeType = "jks";
+                } else {
+                    storeType = fileType;
+                }
+            }
+        }
+        
+        // default to the JVM default type
+        if(storeType == null) {
+            storeType = KeyStore.getDefaultType();
+        }
+        
+        return storeType;
+    }
+    
+    private void checkStore(File store, String password, String storeType, String type) {
         FileInputStream fis = null;
         try {
             fis = new FileInputStream(store);
-            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+            KeyStore ks = KeyStore.getInstance(storeType);
             ks.load(fis, password.toCharArray());
         } catch (GeneralSecurityException e) {
             throw new RuntimeException(type + "store could not be opened, check configuration: "
